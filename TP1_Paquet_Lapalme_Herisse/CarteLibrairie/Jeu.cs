9a870bd28164a1412cc044f2145cad7d06e06324
guillaumepaquet <guillaumepaquet1221@gmail.com>
2019-10-17 09:47:46 -0400

    Merge avec plus de classes dans la librairie

---
+++ TP1_Paquet_Lapalme_Herisse/CarteLibrairie/Jeu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarteLibrairie
+{
+    class Jeu
+    {
+        public event EventHandler<ChangeJoueurEventArgs> ChangeJoueur;
+
+        public Joueur PremierJoueur { get; set; }
+
+        public int ValeurCarteJouer { get; set; }
+
+        public int NombreCarteJouer { get; set; }
+
+        public void ChangerJoueur(Joueur dernierJoueur, Joueur joueurSuivant, int valeurCarte, int nombreCarte)
+        {
+            ChangeJoueurEventArgs eventArgs = new ChangeJoueurEventArgs();
+            eventArgs.PremierJoueur = PremierJoueur;
+            eventArgs.DernierJoueurAJouer = dernierJoueur;
+            eventArgs.JoueurSuivant = joueurSuivant;
+            eventArgs.ValeurCarteJouer = valeurCarte;
+        }
+    }
+}
