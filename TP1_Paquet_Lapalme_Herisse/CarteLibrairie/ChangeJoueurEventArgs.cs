7abba8e0579de7d110452a76eecb2f6d92931b01
guillaumepaquet <guillaumepaquet1221@gmail.com>
2019-10-30 22:13:44 -0400

    Suite dernier commit

--- TP1_Paquet_Lapalme_Herisse/CarteLibrairie/ChangeJoueurEventArgs.cs
+++ TP1_Paquet_Lapalme_Herisse/CarteLibrairie/ChangeJoueurEventArgs.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarteLibrairie
 {
-    class ChangeJoueurEventArgs : EventArgs
+    public class ChangeJoueurEventArgs : EventArgs
     {
         public Joueur PremierJoueur { get; set; }
 
